2f196c6af5b3173e7dc2f1a631125ec34ecf4eda
Deepanshu <indiaboybest@gmail.com>
2024-10-16 23:02:42 +0530

    animated home page

---
+++ script.js
@@ -0,0 +1,16 @@
+let multiText = document.getElementById("changing_text");
+
+let texts = ["Daksh Bist", "Game Developer", "Desiginer"]
+let i = 1;
+
+setInterval(() => {
+    multiText.style.opacity = 0;
+    setTimeout(() => {
+        multiText.innerHTML = texts[i];
+        i++;
+        multiText.style.opacity = 1;
+    },500)
+    if (i > 2) {
+        i = 0
+    }
+}, 3000)
