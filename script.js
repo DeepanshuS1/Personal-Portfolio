6f020f8dbe2020fee2e1bff9c677fbb81a6d1527
Deepanshu <indiaboybest@gmail.com>
2024-10-22 22:31:08 +0530

    js intigrated

--- script.js
+++ script.js
@@ -9,8 +9,30 @@ setInterval(() => {
         multiText.innerHTML = texts[i];
         i++;
         multiText.style.opacity = 1;
-    },500)
+    }, 500)
     if (i > 2) {
         i = 0
     }
 }, 3000)
+
+let sections = document.querySelectorAll("section")
+let homeSec = document.getElementById('home').offsetHeight
+let menuLinks = document.querySelectorAll('header nav a');
+let header = document.querySelector('header')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offSet = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top > offSet && top < offSet + height) {
+            menuLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+        header.classList.toggle('header', window.scrollY > homeSec -10);
+    });
+}
